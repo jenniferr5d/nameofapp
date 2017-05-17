42e015219b0eca219a9d670425eefc364c801b2d
Jennifer Dunne <jenni-dunne@hotmail.com>
2017-05-17 07:45:24 +0200

    3.8 add description of change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,3 +2,13 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+
+class aboutpagescontroller <ApplicationController
+	def about
+	end
+end
+
+class contactpagescontroller <ApplicationController
+	def contact
+	end 
+end
